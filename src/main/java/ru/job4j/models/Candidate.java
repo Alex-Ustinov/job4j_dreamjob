fc5556d5ee4310369bae5884e860a7578bfceea9
18485888 <AAlUstinov@sberbank.ru>
2022-02-17 00:34:32 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/models/Candidate.java
@@ -0,0 +1,6 @@
+package ru.job4j.models;
+
+public class Candidate {
+    private String id;
+    private String name;
+}
---
+++ src/main/java/ru/job4j/models/HR.java
@@ -0,0 +1,6 @@
+package ru.job4j.models;
+
+public class HR {
+    private String id;
+    private String name;
+}
