deb54e69fdda8ca5aa5115bf086e6432856a72d2
18485888 <AAlUstinov@sberbank.ru>
2022-04-05 00:57:07 +0300

    JSP - Java Server Page

--- src/main/java/ru/job4j/models/Candidate.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.models;
-
-public class Candidate {
-    private String id;
-    private String name;
-}
--- src/main/java/ru/job4j/models/HR.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.models;
-
-public class HR {
-    private String id;
-    private String name;
-}
