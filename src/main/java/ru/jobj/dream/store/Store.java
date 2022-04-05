7be1bfe712c1df1615300d8ec085fdad6803d718
18485888 <AAlUstinov@sberbank.ru>
2022-04-05 15:41:46 +0300

    candicates.jsp

--- src/main/java/ru/jobj/dream/store/Store.java
+++ src/main/java/ru/jobj/dream/store/Store.java
@@ -1,6 +1,7 @@
 package ru.jobj.dream.store;
 
 import ru.jobj.dream.model.Post;
+import ru.jobj.dream.model.Candidate;
 
 import java.util.Collection;
 import java.util.Map;
@@ -10,20 +11,29 @@ public class Store {
 
     private static final Store INST = new Store();
 
-    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "01.02.2020", "test"));
         posts.put(2, new Post(2, "Middle Java Job", "15.06.2021", "test1"));
         posts.put(3, new Post(3, "Senior Java Job", "15.09.2022", "test2"));
         posts.put(4, new Post(4, "Lead Java Job", "15.12.2022", "test3"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
