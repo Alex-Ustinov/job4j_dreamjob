5e5126ef512e6d84e705956da57e3f0a3c3f4994
18485888 <AAlUstinov@sberbank.ru>
2022-04-05 12:11:23 +0300

    Scriplet

--- src/main/java/ru/jobj/dream/store/Store.java
+++ src/main/java/ru/jobj/dream/store/Store.java
@@ -16,6 +16,7 @@ public class Store {
         posts.put(1, new Post(1, "Junior Java Job", "01.02.2020", "test"));
         posts.put(2, new Post(2, "Middle Java Job", "15.06.2021", "test1"));
         posts.put(3, new Post(3, "Senior Java Job", "15.09.2022", "test2"));
+        posts.put(4, new Post(4, "Lead Java Job", "15.12.2022", "test3"));
     }
 
     public static Store instOf() {
